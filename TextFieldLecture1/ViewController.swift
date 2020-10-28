46c1f35d46381351015d4503c819dffc174f3428
Christian Carnalla <ccarnalla4514@stu.d214.org>
2020-10-28 14:52:35 -0500

    Textfield Lecture 1

--- TextFieldLecture1/ViewController.swift
+++ TextFieldLecture1/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func whenButtonTapped(_ sender: Any) {
+        let data = textfield.text!
+  print(data)
+    
+    }
+    
 }
 
